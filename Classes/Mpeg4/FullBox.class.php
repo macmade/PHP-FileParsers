3fd40b42f4cab860867bd6a5d25b97cfbce60828
macmade <macmade>
2008-03-13 02:49:55 +0000

    Added code comments for the FullBox abstract. Optimization for the version and flags process, with bitwise operators.

--- Classes/Mpeg4/FullBox.class.php
+++ Classes/Mpeg4/FullBox.class.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * MPEG-4 fullbox atom abstract
+ * 
+ * This abstract class is the base class for all MPEG-4 atom classes based on the fullbox model.
+ * 
+ * SDL from ISO-14496-12:
+ * 
+ * aligned( 8 ) class FullBox( unsigned int( 32 ) boxtype, unsigned int( 8 ) v, bit( 24 ) f ) extends Box( boxtype )
+ * { 
+ *      unsigned int( 8 ) version = v;
+ *      bit( 24 ) flags = f;
+ * }
+ * 
+ * @author          Stéphane Cherpit <stef@eosgarden.com>
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @package         Mpeg4
+ * @version         0.1
+ */
 abstract class Mpeg4_FullBox extends Mpeg4_DataAtom
 {
     /**
@@ -11,18 +30,38 @@ abstract class Mpeg4_FullBox extends Mpeg4_DataAtom
     const DEVEL_STATE    = 'beta';
     const PHP_COMPATIBLE = '5.2.0';
     
+    // Abstract methods
     abstract protected function _processFlags( $rawFlags );
     
+    /**
+     * Process the atom data
+     * 
+     * This method will only take care of the first 32 bits of the atom data.
+     * 8 first bits are for the atom version, and the last 24 ones are for the
+     * atom class. Child classes must call this method (parent::getProcessedData)
+     * in order to gets the version and the flags. The remaining data has to be
+     * processed by the child class
+     * 
+     * @return  object  The processed atom data
+     */
     public function getProcessedData()
     {
+        // Storage for the atom data
         $data = new stdClass();
         
-        $version = unpack( 'N', str_pad( substr( $this->_data, 0, 1 ), 4, chr( 0 ), STR_PAD_LEFT ) );
-        $flags   = unpack( 'N', str_pad( substr( $this->_data, 1, 3 ), 4, chr( 0 ), STR_PAD_LEFT ) );
+        // Gets the first 32 bits from the atom data
+        $unpackData = unpack( 'N', substr( $this->_data, 0, 4 ) );
         
-        $data->version = $version[ 1 ];
-        $data->flags   = $this->_processFlags( $flags[ 1 ] );
+        // Atom version - 8 first bits - Used to know how to handle the data
+        $data->version = $unpackData[ 1 ] & 0xFF000000;
         
+        // Atom flags - 24 last bits
+        $flags         = $unpackData[ 1 ] & 0x00FFFFFF;
+        
+        // Process te atom flags - The method is called from the child class
+        $data->flags   = $this->_processFlags( $flags );
+        
+        // Return the data object
         return $data;
     }
 }
