125aeefa932759ade6eb1c3d0245918bd2c8bbd8
macmade <macmade>
2008-03-13 02:51:30 +0000

    Changed the version constant.

--- Classes/Mpeg4/FullBox.class.php
+++ Classes/Mpeg4/FullBox.class.php
@@ -26,7 +26,7 @@ abstract class Mpeg4_FullBox extends Mpeg4_DataAtom
      * Holds the version, the developpment state
      * and the PHP lower compatible version.
      */
-    const CLASS_VERSION  = '0.1';
+    const CLASS_VERSION  = '0.2';
     const DEVEL_STATE    = 'beta';
     const PHP_COMPATIBLE = '5.2.0';
     
