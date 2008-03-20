fbd2753b73be82b1e2238d40d5233bda4f93ab64
macmade <macmade>
2008-03-20 23:21:07 +0000

    Added the data processing methods to the PDIN atom.

--- Classes/Mpeg4/Atom/Pdin.class.php
+++ Classes/Mpeg4/Atom/Pdin.class.php
@@ -1,6 +1,26 @@
 <?php
 
-final class Mpeg4_Atom_Pdin extends Mpeg4_DataAtom
+/**
+ * MPEG-4 PDIN atom
+ * 
+ * SDL from ISO-14496-12:
+ * 
+ * aligned( 8 ) class ProgressiveDownloadInfoBox extends FullBox( 'pdin', version = 0, 0 )
+ * {
+ *      for ( i = 0; ; i++ ) {
+ *          
+ *          unsigned int( 32 ) rate;
+ *          unsigned int( 32 ) initial_delay;
+ *      }
+ * }
+ * 
+ * @author          Stéphane Cherpit <stef@eosgarden.com>
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @package         Mpeg4/Atom
+ * @version         0.1
+ */
+final class Mpeg4_Atom_Pdin extends Mpeg4_FullBox
 {
     /**
      * Class version constants.
@@ -11,10 +31,48 @@ final class Mpeg4_Atom_Pdin extends Mpeg4_DataAtom
     const DEVEL_STATE    = 'beta';
     const PHP_COMPATIBLE = '5.2.0';
     
+    // Atom type
     protected $_type = 'pdin';
     
-    public function getProcessedData()
+    /**
+     * Process the atom flags
+     * 
+     * @params  string  $rawFlags   The atom raw flags
+     * @return  object  The processed atom flags
+     */
+    protected function _processFlags( $rawFlags )
     {
         return new stdClass();
+    }
+    
+    /**
+     * Process the atom data
+     * 
+     * @return  object  The processed atom data
+     */
+    public function getProcessedData()
+    {
+        // Gets the processed data from the parent (fullbox)
+        $data          = parent::getProcessedData();
+        
+        // Storage for the entries
+        $data->entries = array();
+        
+        // Process each entry
+        for( $i = 4; $i < $this->_dataLength; $i += 8 ) {
+            
+            // Storage for the current entry
+            $entry                = new stdClass();
+            
+            // Process the current entry
+            $entry->rate          = $this->_bigEndianUnsignedLong( $i );
+            $entry->initial_delay = $this->_bigEndianUnsignedLong( $i + 4 );
+            
+            // Stores the current entry
+            $data->entries[]      = $entry;
+        }
+        
+        // Returns the processed data
+        return $data;
     }
 }
