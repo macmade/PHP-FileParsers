b4e84fcbf5975f5d93142bd8389718489a4b5803
macmade <macmade>
2008-03-31 01:15:58 +0000

    Added the data processing methods to the FRMA atom.

--- Classes/Mpeg4/Atom/Frma.class.php
+++ Classes/Mpeg4/Atom/Frma.class.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * MPEG-4 FRMA atom
+ * 
+ * SDL from ISO-14496-12:
+ * 
+ * aligned( 8 ) class OriginalFormatBox( codingname ) extends Box ( 'frma' )
+ * {
+ *      unsigned int( 32 ) data_format = codingname;
+ * }
+ * 
+ * @author          Stéphane Cherpit <stef@eosgarden.com>
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @package         Mpeg4/Atom
+ * @version         0.1
+ */
 final class Mpeg4_Atom_Frma extends Mpeg4_DataAtom
 {
     /**
@@ -11,10 +27,23 @@ final class Mpeg4_Atom_Frma extends Mpeg4_DataAtom
     const DEVEL_STATE    = 'beta';
     const PHP_COMPATIBLE = '5.2.0';
     
+    // Atom type
     protected $_type = 'frma';
     
+    /**
+     * Process the atom data
+     * 
+     * @return  object  The processed atom data
+     */
     public function getProcessedData()
     {
-        return new stdClass();
+        // Data storage
+        $data = new stdClass();
+        
+        // Data format
+        $data->data_format = substr( $this->_data, 0, 4 );
+        
+        // Returns the processed data
+        return $data;
     }
 }
