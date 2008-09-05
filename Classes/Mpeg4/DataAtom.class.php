6d0a6606f038a5b6eb8e499fd3fb2833aea07b67
macmade <macmade>
2008-09-05 15:18:00 +0000

    Added some comments.

--- Classes/Mpeg4/DataAtom.class.php
+++ Classes/Mpeg4/DataAtom.class.php
@@ -17,16 +17,23 @@ abstract class Mpeg4_DataAtom extends Mpeg4_Atom
     protected $_dataLength = 0;
     protected $_final      = false;
     
+    /**
+     * 
+     */
     public function __toString()
     {
+        // Checks if the atom is final
         if( $this->_final ) {
             
+            // Length is set to 0
             $length = pack( 'N', 0 );
             
+            // Returns the whole atom
             return $length . $this->_type . $this->_data;
             
         } elseif( $this->_extended ) {
             
+            // The atom has an extended length
             $length = $this->_dataLength + 16;
             
             $length32 = $length & 0x00001111;
@@ -34,12 +41,15 @@ abstract class Mpeg4_DataAtom extends Mpeg4_Atom
             
             $length = pack( 'N/N', $length64, $length32 );
             
+            // Returns the whole atom
             return  pack( 'N', 1 ) . $this->_type . $length . $this->_data;
             
         } else {
             
+            // Computes the atom length
             $length = pack( 'N', $this->_dataLength + 8 );
             
+            // Returns the whole atom
             return $length . $this->_type . $this->_data;
         }
     }
