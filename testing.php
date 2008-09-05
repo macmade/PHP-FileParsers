dddf54573842609197c317d7a0a2df84d569f854
macmade <macmade>
2008-09-05 16:19:29 +0000

    The test file now hangles MPEG-4 or GIF files.

--- testing.php
+++ testing.php
@@ -1,5 +1,5 @@
 <?php
-    require_once( 'Classes/Mpeg4/Init.class.php' );
+    require_once( './init.inc.php' );
 ?>
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -11,44 +11,65 @@
     <link rel="stylesheet" href="Css/base.css" type="text/css" media="screen" charset="utf-8" />
 </head>
 <body>
-    <h1>MPEG-4 Parser</h1>
-    <div class="mpeg4-upload">
-        <form method="post" enctype="multipart/form-data" id="mpeg4-upload-form">
-            <label for="mpeg4-file">MPEG-4 file:</label><br />
-            <input name="mpeg4-file" id="mpeg4-file" type="file" /><br />
-            <input name="mpeg4-submit" id="mpeg4-submit" type="submit" value="Analyze MPEG-4 file" />
+    <h1>File Parser</h1>
+    <div class="upload">
+        <form method="post" enctype="multipart/form-data" id="upload-form">
+            <label for="file">The file to upload:</label><br />
+            <input name="file" id="file" type="file" /><br />
+            <input name="submit" id="submit" type="submit" value="Analyze the file" />
         </form>
     </div>
     <?php
-        if( isset( $_POST[ 'mpeg4-submit' ] ) && isset( $_FILES[ 'mpeg4-file' ] ) && is_array( $_FILES[ 'mpeg4-file' ] ) && count( $_FILES[ 'mpeg4-file' ] ) ) {
+        if( isset( $_POST[ 'submit' ] ) && isset( $_FILES[ 'file' ] ) && is_array( $_FILES[ 'file' ] ) && count( $_FILES[ 'file' ] ) ) {
             
-            $file = $_FILES[ 'mpeg4-file' ];
+            $file = $_FILES[ 'file' ];
             
-            if( $file[ 'type' ] == 'video/mp4' ) {
+            switch( $file[ 'type' ] ) {
                 
-                $parser    = new Mpeg4_Parser( $file[ 'tmp_name' ], true );
-                $mpeg4File = $parser->getMpeg4File();
-                $dataArray = $mpeg4File->getProcessedData();
-                $warnings  = $parser->getWarnings();
-                
-                if( count( $warnings ) ) {
+                case 'video/mp4':
                     
-                    print '<h2>Warnings</h2>';
+                    $parser    = new Mpeg4_Parser( $file[ 'tmp_name' ], true );
+                    $mpeg4File = $parser->getMpeg4File();
+                    $dataArray = $mpeg4File->getProcessedData();
+                    $warnings  = $parser->getWarnings();
+                    
+                    if( count( $warnings ) ) {
+                        
+                        print '<h2>Warnings</h2>';
+                        print '<pre>';
+                        print_r( $warnings );
+                        print '</pre>';
+                    }
+                    
+                    print '<h2>Parsed data</h2>';
                     print '<pre>';
-                    print_r( $warnings );
+                    print_r( $dataArray );
                     print '</pre>';
-                }
+                    
+                    file_put_contents( 'Movies/debug.mp4', $mpeg4File );
+                    break;
                 
-                print '<h2>Parsed data</h2>';
-                print '<pre>';
-                print_r( $dataArray );
-                print '</pre>';
+                case 'image/gif':
+                    
+                    try {
+                        
+                        $parser    = new Gif_Parser( $file[ 'tmp_name' ] );
+                        
+                        print '<h2>Parsed data</h2>';
+                        print '<pre>';
+                        print_r( $parser->getInfos() );
+                        print '</pre>';
+                        
+                    } catch( Exception $e ) {
+                        
+                        print '<div class="error">' . $e->getMessage() .'</div>';
+                    }
+                    break;
                 
-                file_put_contents( 'Movies/debug.mp4', $mpeg4File );
-                
-            } else {
-                
-                print '<div class="error">Sorry, the file is not an MPEG-4 movie</div>';
+                default:
+                    
+                    print '<div class="error">Sorry, the file type is not recognized or supported</div>';
+                    break;
             }
         }
     ?>
