4817152af43d50edf64824f09e3adc0f511ef2f3
macmade <macmade>
2008-09-06 04:36:52 +0000

    Added an instance of the binary utilities class and removes the binary utilities methods.

--- Classes/Mpeg4/Atom.class.php
+++ Classes/Mpeg4/Atom.class.php
@@ -46,126 +46,58 @@ abstract class Mpeg4_Atom
     abstract public function __toString();
     abstract public function getLength();
     
-    private static $_dividers = array(
-        2  => 4,            // 1 << 2  - (2 ** 2)  - For the 30.2 fixed point numbers
-        8  => 256,          // 1 << 8  - (2 ** 8)  - For the 8.8 fixed point numbers
-        16 => 65536,        // 1 << 16 - (2 ** 16) - For the 16.16 fixed point numbers
-        30 => 1073741824    // 1 << 30 - (2 ** 30) - For the 2.30 fixed point numbers
-    );
+    /**
+     * The instance of the binary utilities class
+     */
+    protected static $_binUtils = NULL;
+    
+    /**
+     * Wether the static variables are set or not
+     */
+    protected static $_hasStatic = false;
     
     /**
      * The atom type
      */
-    protected $_type         = '';
+    protected $_type             = '';
     
     /**
      * Wether the atom has an extended length
      */
-    protected $_extended     = false;
+    protected $_extended         = false;
     
     /**
      * The parent atom, if any
      */
-    protected $_parent       = NULL;
+    protected $_parent           = NULL;
     
     /**
-     * Gets a big endian unsigned long from the atom data
+     * Class constructor
      * 
-     * @param   int     The beginning of the field in the atom data
-     * @return  float   The big endian unsigned long
+     * @return  NULL
      */
-    protected function _bigEndianUnsignedLong( $dataOffset )
+    public function __construct()
     {
-        // Gets the unsigned long - big endian from the atom data
-        $unpackData = unpack( 'N', substr( $this->_data, $dataOffset, 4 ) );
-        
-        // Return the big endian unsigned long
-        return $unpackData[ 1 ];
-    }
-    
-    /**
-     * Gets a big endian unsigned short from the atom data
-     * 
-     * @param   int     The beginning of the field in the atom data
-     * @return  float   The big endian unsigned short
-     */
-    protected function _bigEndianUnsignedShort( $dataOffset )
-    {
-        // Gets the unsigned long - big endian from the atom data
-        $unpackData = unpack( 'n', substr( $this->_data, $dataOffset, 2 ) );
-        
-        // Return the big endian unsigned long
-        return $unpackData[ 1 ];
-    }
-    
-    /**
-     * Gets a fixed point number from the atom data
-     * 
-     * Actually, only 8.8, 16.16, 30.2 and 2.30 fixed point formats are supported.
-     * 
-     * @param   int     The beginning of the fixed point number field in the atom data
-     * @param   int     The number of bits for the integer part (2, 8, 16 or 30)
-     * @param   int     The number of bits for the fractional part (2, 8, 16 or 30)
-     * @return  float   The fixed point number
-     */
-    protected function _bigEndianFixedPoint( $dataOffset, $integerLength, $fractionalLength )
-    {
-        // Checks if the fixed point number is expressed on 16 or 32 bits
-        if( $integerLength + $fractionalLength === 16 ) {
+        // Checks if the static variables are set
+        if( !self::$_hasStatic ) {
             
-            // Unsigned short - big endian
-            $unpackFormat   = 'n';
-            
-            // Mask for the fractional part
-            $fractionalMask = 0x00FF;
-            
-            // Length of the data to read
-            $dataLength     = 2;
-            
-        } else {
-            
-            // Unsigned long - big endian
-            $unpackFormat   = 'N';
-            
-            // Mask for the fractional part
-            $fractionalMask = 0x0000FFFF;
-            
-            // Length of the data to read
-            $dataLength     = 4;
+            // Sets the static variables
+            self::_setStaticVars();
         }
-        
-        // Gets the decimal value for the fixed point number from the atom data
-        $unpackData = unpack( $unpackFormat, substr( $this->_data, $dataOffset, $dataLength ) );
-        
-        // Computes the integer part
-        $integer    = $unpackData[ 1 ] >> $fractionalLength;
-        
-        // Computes the fractional part
-        $fractional = ( $unpackData[ 1 ] & $fractionalMask ) / self::$_dividers[ $fractionalLength ];
-        
-        // Returns the fixed point number
-        return $integer + $fractional;
     }
     
     /**
-     * Gets an ISO-639-2 language code from the atom data
+     * Sets the needed static variables
      * 
-     * @param   int     The beginning of the language code field in the atom data (pad bit included, so 16 bits!)
-     * @return  string  The ISO-639-2 language code
+     * @return  NULL
      */
-    protected function _bigEndianIso639Code( $dataOffset )
+    protected static function _setStaticVars()
     {
-        // Gets an big endian unsigned short (16 bits) from the given data offset
-        $unpackData = unpack( 'n', substr( $this->_data, $dataOffset, 2 ) );
+        // Gets the instance of the binary utilities class
+        self::$_binUtils  = Binary_Utils::getInstance();
         
-        // Gets letters (each letter is coded on 5 bits)
-        $letter1 = ( $unpackData[ 1 ] & 0x7C00 ) >> 10;  // Mask is 0111 1100 0000 0000
-        $letter2 = ( $unpackData[ 1 ] & 0x03E0 ) >> 5;   // Mask is 0000 0011 1110 0000
-        $letter3 = ( $unpackData[ 1 ] & 0x001F );        // Mask is 0000 0000 0001 1111
-        
-        // Returns the language code as a string
-        // 0x60 - 96 is added to each letter, as the language codes are lowercase letters
-        return chr( $letter1 + 0x60 ) . chr( $letter2 + 0x60 ) . chr( $letter3 + 0x60 );
+        // Static variables are set
+        self::$_hasStatic = true;
     }
     
     /**
