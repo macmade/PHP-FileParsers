764119899f2e85c076ee3f2766eb78710ef25b3e
macmade <macmade>
2008-09-06 20:57:07 +0000

    Added the instance of the binary utilities class to the PNG chunk abstract.

--- Classes/Png/Chunk.class.php
+++ Classes/Png/Chunk.class.php
@@ -19,22 +19,64 @@ abstract class Png_Chunk
     const DEVEL_STATE    = 'alpha';
     const PHP_COMPATIBLE = '5.2.0';
     
+    /**
+     * The abstract method used to get the processed chunk data
+     */
     abstract public function getProcessedData();
+    
+    /**
+     * The instance of the binary utilities class
+     */
+    protected static $_binUtils  = NULL;
+    
+    /**
+     * Wether the static variables are set or not
+     */
+    protected static $_hasStatic = false;
     
     /**
      * The chunk type
      */
-    protected $_type       = '';
+    protected $_type             = '';
     
     /**
      * The chunk data
      */
-    protected $_data       = '';
+    protected $_data             = '';
     
     /**
      * The chunk data length
      */
-    protected $_dataLength = 0;
+    protected $_dataLength       = 0;
+    
+    /**
+     * Class constructor
+     * 
+     * @return  NULL
+     */
+    public function __construct()
+    {
+        // Checks if the static variables are set
+        if( !self::$_hasStatic ) {
+            
+            // Sets the static variables
+            self::_setStaticVars();
+        }
+    }
+    
+    /**
+     * Sets the needed static variables
+     * 
+     * @return  NULL
+     */
+    protected static function _setStaticVars()
+    {
+        // Gets the instance of the binary utilities class
+        self::$_binUtils  = Binary_Utils::getInstance();
+        
+        // Static variables are set
+        self::$_hasStatic = true;
+    }
     
     /**
      * 
