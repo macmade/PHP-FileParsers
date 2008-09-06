2a9afd99a8302045f5a7ccd7028b484545e14b9e
macmade <macmade>
2008-09-06 06:53:02 +0000

    Added an exception class for the singleton classes.

---
+++ Classes/Singleton/Exception.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Exception class for the singleton classes
+ * 
+ * @author          Jean-David Gadina <macmade@eosgarden.com>
+ * @copyright       Copyright &copy; 2008
+ * @package         Singleton
+ * @version         0.1
+ */
+class Singleton_Exception extends Exception_Base
+{}
