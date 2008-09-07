56c2d2f1936174d11efdc772f0748b26a023678c
macmade <macmade>
2008-09-07 20:54:19 +0000

    The tRNS chunk should now be working...

--- Classes/Png/Chunk/Trns.class.php
+++ Classes/Png/Chunk/Trns.class.php
@@ -41,37 +41,47 @@ class Png_Chunk_Trns extends Png_Chunk
     public function getProcessedData()
     {
         // Storage
-        $data                                = new stdClass();
-        $data->colourType0                   = new stdClass();
-        $data->colourType2                   = new stdClass();
-        $data->colourType3                   = array();
+        $data     = new stdClass();
         
-        // Gets the value for the coulour type 0
-        $data->colourType0->greySampleValue  = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 0 );
+        // Gets the IHDR chunk
+        $ihdr     = $this->_pngFile->IHDR;
         
-        // Gets the values for the coulour type 2
-        $data->colourType2->redSampleValue   = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 2 );
-        $data->colourType2->greenSampleValue = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 4 );
-        $data->colourType2->blueSampleValue  = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 6 );
+        // Process the IHDR data
+        $ihdrData = $ihdr->getProcessedData();
         
-        // Gets the hexadecimal values
-        $redHex                              = dechex( $data->colourType0->redSampleValue );
-        $greenHex                            = dechex( $data->colourType0->greenSampleValue );
-        $blueHex                             = dechex( $data->colourType0->blueSampleValue );
-        
-        // Completes each hexadecimal value if needed
-        $redHex                              = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
-        $greenHex                            = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
-        $blueHex                             = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
-        
-        // Adds the hexadecimal color value to colour type 2
-        $data->colourType2->hex              = '#' . strtoupper( $redHex . $greenHex . $blueHex );
-        
-        // Process the remaining chunk data till the end
-        for( $i = 8; $i < $this->_dataLength; $i++ ) {
+        // Checks the data length
+        switch( $ihdrData->colourType ) {
             
-            // Gets the alpha index
-            $data->colourType3[] = self::$_binUtils->unsignedChar( $this->_data, $i );
+            // Greyscale
+            case 0:
+                
+                // Gets the sample value
+                $data->greySampleValue = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 0 );
+                break;
+            
+            // RGB
+            case 2:
+                
+                // Gets the sample values
+                $data->redSampleValue   = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 0 );
+                $data->greenSampleValue = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 2 );
+                $data->blueSampleValue  = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 4 );
+                break;
+            
+            // Indexed color
+            case 3:
+                
+                // Storage
+                $data->alphasForPaletteIndexes = array();
+                
+                // Process the chunk data till the end
+                for( $i = 0; $i < $this->_dataLength; $i++ ) {
+                    
+                    // Gets the alpha for the current palette index
+                    $data->alphasForPaletteIndexes[] = self::$_binUtils->unsignedChar( $this->_data, $i );
+                }
+                
+                break;
         }
         
         // Returns the processed data
