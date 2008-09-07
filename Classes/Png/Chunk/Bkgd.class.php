f0e154b309a279bd5269e00afd2d45fdc106e7c1
macmade <macmade>
2008-09-07 20:24:24 +0000

    The bKGD chunk should now be working.

--- Classes/Png/Chunk/Bkgd.class.php
+++ Classes/Png/Chunk/Bkgd.class.php
@@ -26,7 +26,7 @@ class Png_Chunk_Bkgd extends Png_Chunk
     /**
      * The chunk type
      */
-    protected $_type = 'bKGd';
+    protected $_type = 'bKGD';
     
     /**
      * Process the chunk data
@@ -41,45 +41,114 @@ class Png_Chunk_Bkgd extends Png_Chunk
     public function getProcessedData()
     {
         // Storage
-        $data                                = new stdClass();
-        $data->colourType0                   = new stdClass();
-        $data->colourType2                   = new stdClass();
-        $data->colourType3                   = new stdClass();
-        $data->colourType4                   = new stdClass();
-        $data->colourType6                   = new stdClass();
+        $data = new stdClass();
         
-        // Gets the value for the coulour type 0
-        $data->colourType0->greyscale        = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 0 );
-        
-        // Colour type 4 is the same as coulour type 0
-        $data->colourType4->greyscale        = $data->colourType0->greyscale;
-        
-        // Gets the values for the coulour type 2
-        $data->colourType2->redSampleValue   = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 2 );
-        $data->colourType2->greenSampleValue = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 4 );
-        $data->colourType2->blueSampleValue  = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 6 );
-        
-        // Gets the hexadecimal values
-        $redHex                              = dechex( $data->colourType2->redSampleValue );
-        $greenHex                            = dechex( $data->colourType2->greenSampleValue );
-        $blueHex                             = dechex( $data->colourType2->blueSampleValue );
-        
-        // Completes each hexadecimal value if needed
-        $redHex                              = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
-        $greenHex                            = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
-        $blueHex                             = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
-        
-        // Adds the hexadecimal color value to colour type 2
-        $data->colourType2->hex              = '#' . strtoupper( $redHex . $greenHex . $blueHex );
-        
-        // Colour type 6 is the same as coulour type 2
-        $data->colourType6->redSampleValue   = $data->colourType2->redSampleValue;
-        $data->colourType6->greenSampleValue = $data->colourType2->greenSampleValue;
-        $data->colourType6->blueSampleValue  = $data->colourType2->blueSampleValue;
-        $data->colourType6->hex              = $data->colourType2->hex;
-        
-        // Gets the value for the coulour type 0
-        $data->colourType3->paletteIndex     = self::$_binUtils->unsignedChar( $this->_data, 8 );
+        // Checks the data length
+        switch( $this->_dataLength ) {
+            
+            // Indexed color
+            case 1:
+                
+                // Sets the colour type
+                $data->colourType   = 3;
+                
+                // Gets the palette index
+                $data->paletteIndex = self::$_binUtils->unsignedChar( $this->_data );
+                break;
+            
+            // Greyscale
+            case 2:
+                
+                // Gets the IHDR chunk
+                $ihdr     = $this->_pngFile->IHDR;
+                
+                // Process the IHDR data
+                $ihdrData = $ihdr->getProcessedData();
+                
+                // Checks the bit depth
+                if( $ihdrData->bitDepth === 8 ) {
+                    
+                    // Sets the colour type
+                    $data->colourType = 0;
+                    
+                    // Gets the greyscale value
+                    $data->greyscale  = self::$_binUtils->unsignedChar( $this->_data, 1 );
+                    
+                } elseif( $ihdrData->bitDepth === 16 ) {
+                    
+                    // Sets the colour type
+                    $data->colourType   = 4;
+                    
+                    // Gets the greyscale and the alpha value
+                    $data->greyscale      = self::$_binUtils->unsignedChar( $this->_data, 0 );
+                    $data->greyscaleAlpha = self::$_binUtils->unsignedChar( $this->_data, 1 );
+                }
+                
+                break;
+            
+            // RGB
+            case 6:
+                
+                // Gets the IHDR chunk
+                $ihdr     = $this->_pngFile->IHDR;
+                
+                // Process the IHDR data
+                $ihdrData = $ihdr->getProcessedData();
+                
+                // Checks the bit depth
+                if( $ihdrData->bitDepth === 8 ) {
+                    
+                    // Sets the colour type
+                    $data->colourType   = 2;
+                    
+                    // Gets the color values
+                    $data->red   = self::$_binUtils->unsignedChar( $this->_data, 1 );
+                    $data->green = self::$_binUtils->unsignedChar( $this->_data, 3 );
+                    $data->blue  = self::$_binUtils->unsignedChar( $this->_data, 5 );
+                    
+                    // Gets the hexadecimal values
+                    $redHex           = dechex( $data->red );
+                    $greenHex         = dechex( $data->green );
+                    $blueHex          = dechex( $data->blue );
+                    
+                    // Completes each hexadecimal value if needed
+                    $redHex           = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
+                    $greenHex         = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
+                    $blueHex          = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
+                    
+                    // Adds the hexadecimal color value to colour type 2
+                    $data->hex        = '#' . strtoupper( $redHex . $greenHex . $blueHex );
+                    
+                } elseif( $ihdrData->bitDepth === 16 ) {
+                    
+                    // Sets the colour type
+                    $data->colourType   = 6;
+                    
+                    // Gets the color and the alpha values
+                    $data->red        = self::$_binUtils->unsignedChar( $this->_data, 0 );
+                    $data->redAlpha   = self::$_binUtils->unsignedChar( $this->_data, 1 );
+                    $data->green      = self::$_binUtils->unsignedChar( $this->_data, 2 );
+                    $data->greenAlpha = self::$_binUtils->unsignedChar( $this->_data, 3 );
+                    $data->blue       = self::$_binUtils->unsignedChar( $this->_data, 4 );
+                    $data->blueAlpha  = self::$_binUtils->unsignedChar( $this->_data, 5 );
+                    
+                    // Gets the hexadecimal values
+                    $redHex           = dechex( $data->red );
+                    $greenHex         = dechex( $data->green );
+                    $blueHex          = dechex( $data->blue );
+                    
+                    // Completes each hexadecimal value if needed
+                    $redHex           = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
+                    $greenHex         = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
+                    $blueHex          = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
+                    
+                    // Adds the hexadecimal color value to colour type 2
+                    $data->hex        = '#' . strtoupper( $redHex . $greenHex . $blueHex );
+                }
+                
+                break;
+            
+        }
         
         // Returns the processed data
         return $data;
