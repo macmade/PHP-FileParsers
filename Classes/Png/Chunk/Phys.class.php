0218ffcef202d1b558109abea48c0a2e199b4abe
macmade <macmade>
2008-09-07 16:13:32 +0000

    Added the method to process the data of the pHYs chunk.

--- Classes/Png/Chunk/Phys.class.php
+++ Classes/Png/Chunk/Phys.class.php
@@ -29,6 +29,17 @@ class Png_Chunk_Phys extends Png_Chunk
      */
     public function getProcessedData()
     {
-        return false;
+        // Storage
+        $data                = new stdClass();
+        
+        // Gets the pixel aspect ratio
+        $data->pixelPerUnitX = self::$_binUtils->bigEndianUnsignedLong( $this->_data, 0 );
+        $data->pixelPerUnitY = self::$_binUtils->bigEndianUnsignedLong( $this->_data, 4 );
+        
+        // Gets the unit
+        $data->unit          = self::$_binUtils->unsignedChar( $this->_data, 8 );
+        
+        // Returns the processed data
+        return $data;
     }
 }
