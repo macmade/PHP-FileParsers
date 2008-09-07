faa531a3efe78b95fa1580da4f0e9180cd6cbddf
macmade <macmade>
2008-09-07 00:19:48 +0000

    Added the method to process the data of the sRGB chunk.

--- Classes/Png/Chunk/Srgb.class.php
+++ Classes/Png/Chunk/Srgb.class.php
@@ -20,6 +20,14 @@ class Png_Chunk_Srgb extends Png_Chunk
     const PHP_COMPATIBLE = '5.2.0';
     
     /**
+     * Rendering intent values
+     */
+    const RGB_PERCEPTUAL            = 0x0;
+    const RGB_RELATIVE_COLORIMETRIC = 0x1;
+    const RGB_SATURATION            = 0x2;
+    const RGB_ABSOLUTE_COLORIMETRIC = 0x3;
+    
+    /**
      * The chunk type
      */
     protected $_type = 'sRGB';
@@ -29,6 +37,19 @@ class Png_Chunk_Srgb extends Png_Chunk
      */
     public function getProcessedData()
     {
-        return false;
+        // Storage
+        $data                  = new stdClass();
+        
+        // Gets the rendering intent
+        $data->renderingIntent = self::$_binUtils->unsignedChar( $this->_data );
+        
+        // Stores the human readable rendering intent
+        $data->perceptual           = ( $data->renderingIntent === self::RGB_PERCEPTUAL )            ? true : false;
+        $data->relativeColorimetric = ( $data->renderingIntent === self::RGB_RELATIVE_COLORIMETRIC ) ? true : false;
+        $data->saturation           = ( $data->renderingIntent === self::RGB_SATURATION )            ? true : false;
+        $data->absoluteColorimetric = ( $data->renderingIntent === self::RGB_ABSOLUTE_COLORIMETRIC ) ? true : false;
+        
+        // Returns the processed data
+        return $data;
     }
 }
