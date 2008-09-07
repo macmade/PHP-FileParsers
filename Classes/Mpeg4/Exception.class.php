f2cf8e8e039bceef153e5d88bd7781b38186cd62
macmade <macmade>
2008-09-07 14:47:20 +0000

    Added the requested class constants to the exception classes.

--- Classes/Mpeg4/Exception.class.php
+++ Classes/Mpeg4/Exception.class.php
@@ -11,6 +11,15 @@
 class Mpeg4_Exception extends Exception_Base
 {
     /**
+     * Class version constants.
+     * Holds the version, the developpment state
+     * and the PHP lower compatible version.
+     */
+    const CLASS_VERSION  = '0.1';
+    const DEVEL_STATE    = 'alpha';
+    const PHP_COMPATIBLE = '5.2.0';
+    
+    /**
      * Error codes for the exceptions
      */
     const EXCEPTION_INVALID_ATOM = 0x01;
