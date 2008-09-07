11f057da7551ad1999301384b82b6e6c81fe5757
macmade <macmade>
2008-09-07 17:15:48 +0000

    Added the method to process the data of the tRNs chunk.

--- Classes/Png/Chunk/Trns.class.php
+++ Classes/Png/Chunk/Trns.class.php
@@ -40,6 +40,41 @@ class Png_Chunk_Trns extends Png_Chunk
      */
     public function getProcessedData()
     {
-        return new stdClass();
+        // Storage
+        $data                                = new stdClass();
+        $data->colourType0                   = new stdClass();
+        $data->colourType2                   = new stdClass();
+        $data->colourType3                   = array();
+        
+        // Gets the value for the coulour type 0
+        $data->colourType0->greySampleValue  = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 0 );
+        
+        // Gets the values for the coulour type 2
+        $data->colourType0->redSampleValue   = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 2 );
+        $data->colourType0->greenSampleValue = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 4 );
+        $data->colourType0->blueSampleValue  = self::$_binUtils->bigEndianUnsignedShort( $this->_data, 6 );
+        
+        // Gets the hexadecimal values
+        $redHex                              = dechex( $data->colourType0->redSampleValue );
+        $greenHex                            = dechex( $data->colourType0->greenSampleValue );
+        $blueHex                             = dechex( $data->colourType0->blueSampleValue );
+        
+        // Completes each hexadecimal value if needed
+        $redHex                              = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
+        $greenHex                            = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
+        $blueHex                             = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
+        
+        // Adds the hexadecimal color value to colour type 2
+        $data->colourType0->hex              = '#' . strtoupper( $redHex . $greenHex . $blueHex );
+        
+        // Process the remaining chunk data till the end
+        for( $i = 8; $i < $this->_dataLength; $i++ ) {
+            
+            // Gets the alpha index
+            $data->colourType3[] = self::$_binUtils->unsignedChar( $this->_data, $i );
+        }
+        
+        // Returns the processed data
+        return $data;
     }
 }
