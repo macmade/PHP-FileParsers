7e8d5303d1bde163061ff86e403b1b58bf19761b
macmade <macmade>
2008-09-07 20:39:54 +0000

    Another version. Guess I've now understood how to deal with this kind of chunks... ;)

--- Classes/Png/Chunk/Bkgd.class.php
+++ Classes/Png/Chunk/Bkgd.class.php
@@ -41,13 +41,48 @@ class Png_Chunk_Bkgd extends Png_Chunk
     public function getProcessedData()
     {
         // Storage
-        $data = new stdClass();
+        $data     = new stdClass();
+        
+        // Gets the IHDR chunk
+        $ihdr     = $this->_pngFile->IHDR;
+        
+        // Process the IHDR data
+        $ihdrData = $ihdr->getProcessedData();
         
         // Checks the data length
-        switch( $this->_dataLength ) {
+        switch( $ihdrData->colourType ) {
+            
+            // Greyscale
+            case 0:
+                
+                // Gets the greyscale value
+                $data->greyscale = self::$_binUtils->unsignedChar( $this->_data, 1 );
+                break;
+            
+            // RGB
+            case 2:
+                
+                // Gets the color values
+                $data->red   = self::$_binUtils->unsignedChar( $this->_data, 1 );
+                $data->green = self::$_binUtils->unsignedChar( $this->_data, 3 );
+                $data->blue  = self::$_binUtils->unsignedChar( $this->_data, 5 );
+                
+                // Gets the hexadecimal values
+                $redHex      = dechex( $data->red );
+                $greenHex    = dechex( $data->green );
+                $blueHex     = dechex( $data->blue );
+                
+                // Completes each hexadecimal value if needed
+                $redHex      = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
+                $greenHex    = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
+                $blueHex     = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
+                
+                // Adds the hexadecimal color value to colour type 2
+                $data->hex   = '#' . strtoupper( $redHex . $greenHex . $blueHex );
+                break;
             
             // Indexed color
-            case 1:
+            case 3:
                 
                 // Sets the colour type
                 $data->colourType   = 3;
@@ -56,96 +91,37 @@ class Png_Chunk_Bkgd extends Png_Chunk
                 $data->paletteIndex = self::$_binUtils->unsignedChar( $this->_data );
                 break;
             
-            // Greyscale
-            case 2:
+            // Greyscale with alpha
+            case 4:
                 
-                // Gets the IHDR chunk
-                $ihdr     = $this->_pngFile->IHDR;
-                
-                // Process the IHDR data
-                $ihdrData = $ihdr->getProcessedData();
-                
-                // Checks the bit depth
-                if( $ihdrData->bitDepth === 8 ) {
-                    
-                    // Sets the colour type
-                    $data->colourType = 0;
-                    
-                    // Gets the greyscale value
-                    $data->greyscale  = self::$_binUtils->unsignedChar( $this->_data, 1 );
-                    
-                } elseif( $ihdrData->bitDepth === 16 ) {
-                    
-                    // Sets the colour type
-                    $data->colourType   = 4;
-                    
-                    // Gets the greyscale and the alpha value
-                    $data->greyscale      = self::$_binUtils->unsignedChar( $this->_data, 0 );
-                    $data->greyscaleAlpha = self::$_binUtils->unsignedChar( $this->_data, 1 );
-                }
-                
+                // Gets the greyscale and the alpha value
+                $data->greyscale      = self::$_binUtils->unsignedChar( $this->_data, 0 );
+                $data->greyscaleAlpha = self::$_binUtils->unsignedChar( $this->_data, 1 );
                 break;
             
-            // RGB
+            // RGB with alpha
             case 6:
                 
-                // Gets the IHDR chunk
-                $ihdr     = $this->_pngFile->IHDR;
+                // Gets the color and the alpha values
+                $data->red        = self::$_binUtils->unsignedChar( $this->_data, 0 );
+                $data->redAlpha   = self::$_binUtils->unsignedChar( $this->_data, 1 );
+                $data->green      = self::$_binUtils->unsignedChar( $this->_data, 2 );
+                $data->greenAlpha = self::$_binUtils->unsignedChar( $this->_data, 3 );
+                $data->blue       = self::$_binUtils->unsignedChar( $this->_data, 4 );
+                $data->blueAlpha  = self::$_binUtils->unsignedChar( $this->_data, 5 );
                 
-                // Process the IHDR data
-                $ihdrData = $ihdr->getProcessedData();
+                // Gets the hexadecimal values
+                $redHex           = dechex( $data->red );
+                $greenHex         = dechex( $data->green );
+                $blueHex          = dechex( $data->blue );
                 
-                // Checks the bit depth
-                if( $ihdrData->bitDepth === 8 ) {
-                    
-                    // Sets the colour type
-                    $data->colourType   = 2;
-                    
-                    // Gets the color values
-                    $data->red   = self::$_binUtils->unsignedChar( $this->_data, 1 );
-                    $data->green = self::$_binUtils->unsignedChar( $this->_data, 3 );
-                    $data->blue  = self::$_binUtils->unsignedChar( $this->_data, 5 );
-                    
-                    // Gets the hexadecimal values
-                    $redHex           = dechex( $data->red );
-                    $greenHex         = dechex( $data->green );
-                    $blueHex          = dechex( $data->blue );
-                    
-                    // Completes each hexadecimal value if needed
-                    $redHex           = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
-                    $greenHex         = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
-                    $blueHex          = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
-                    
-                    // Adds the hexadecimal color value to colour type 2
-                    $data->hex        = '#' . strtoupper( $redHex . $greenHex . $blueHex );
-                    
-                } elseif( $ihdrData->bitDepth === 16 ) {
-                    
-                    // Sets the colour type
-                    $data->colourType   = 6;
-                    
-                    // Gets the color and the alpha values
-                    $data->red        = self::$_binUtils->unsignedChar( $this->_data, 0 );
-                    $data->redAlpha   = self::$_binUtils->unsignedChar( $this->_data, 1 );
-                    $data->green      = self::$_binUtils->unsignedChar( $this->_data, 2 );
-                    $data->greenAlpha = self::$_binUtils->unsignedChar( $this->_data, 3 );
-                    $data->blue       = self::$_binUtils->unsignedChar( $this->_data, 4 );
-                    $data->blueAlpha  = self::$_binUtils->unsignedChar( $this->_data, 5 );
-                    
-                    // Gets the hexadecimal values
-                    $redHex           = dechex( $data->red );
-                    $greenHex         = dechex( $data->green );
-                    $blueHex          = dechex( $data->blue );
-                    
-                    // Completes each hexadecimal value if needed
-                    $redHex           = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
-                    $greenHex         = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
-                    $blueHex          = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
-                    
-                    // Adds the hexadecimal color value to colour type 2
-                    $data->hex        = '#' . strtoupper( $redHex . $greenHex . $blueHex );
-                }
+                // Completes each hexadecimal value if needed
+                $redHex           = ( strlen( $redHex )   == 1 ) ? '0' . $redHex   : $redHex;
+                $greenHex         = ( strlen( $greenHex ) == 1 ) ? '0' . $greenHex : $greenHex;
+                $blueHex          = ( strlen( $blueHex )  == 1 ) ? '0' . $blueHex  : $blueHex;
                 
+                // Adds the hexadecimal color value to colour type 2
+                $data->hex        = '#' . strtoupper( $redHex . $greenHex . $blueHex );
                 break;
             
         }
